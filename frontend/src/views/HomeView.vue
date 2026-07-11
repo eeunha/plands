6536e223feca518788e2524c0a1d18f706b6faf3
eeunha <eunha.dev@gmail.com>
2026-07-11 23:49:22 +0900

    Feat: HomeView 로그인 분기 및 캘린더 페이지 이동
    - 로그인 상태에 따라 캘린더 뷰 리다이렉트 구현
    - HomeView 레이아웃 스타일 개선 (하단 스크롤 제거)

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home-container">
     <div v-if="isLoggedIn">
-      <p>캘린더 component가 들어가야함...</p>
+      <CalendarView />
     </div>
     <div v-else>
       <Introduce />
@@ -10,33 +10,49 @@
 </template>
 
 <script>
-import Introduce from '@/components/Introduce.vue';
+import Introduce from '@/components/Introduce.vue'
 import { useAuthStore } from '@/stores/authStore.js'
+import CalendarView from '@/views/CalendarView.vue'
 export default {
-  name: "HomeView",
-  data() {
-    return {
-
+  name: 'HomeView',
+  created() {
+    const authStore = useAuthStore()
+    if (authStore.isLoggedIn) {
+      this.$router.replace('/calendar')
     }
+  },
+  data() {
+    return {}
   },
   computed: {
     isLoggedIn() {
-      const authStore = useAuthStore();
-      return authStore.isLoggedIn;
-    }
+      const authStore = useAuthStore()
+      return authStore.isLoggedIn
+    },
   },
   components: {
-    Introduce
+    CalendarView,
+    Introduce,
   },
-
 }
 </script>
 
 <style scoped>
+:global(html, body) {
+  margin: 0;
+  padding: 0;
+  overflow: hidden; /* 스크롤 강제 차단 */
+}
+
 .home-container {
   width: 100vw;
   height: 100vh;
   overflow: hidden;
+  display: flex; /* flex로 내부 요소 높이 강제 조정 */
+  flex-direction: column;
 }
 
+.home-container > div {
+  flex: 1;
+}
 </style>

022f793e445f8167bda94d6c08e690ec0ddfbf37
eeunha <eunha.dev@gmail.com>
2026-07-14 17:22:42 +0900

    Fix: HomeView 전역 스타일 오염 제거 및 레이아웃 최적화
    - HomeView에 추가되었던 :global 스타일이 타 페이지의 스크롤까지 제한하는 이슈를 발견하여, 전역 스타일 오염을 방지하기 위해 해당 스타일을 제거하고 원복함.
    - 기존 리다이렉트 로직을 유지하여 스크롤 문제를 근본적으로 제어.

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -38,21 +38,9 @@ export default {
 </script>
 
 <style scoped>
-:global(html, body) {
-  margin: 0;
-  padding: 0;
-  overflow: hidden; /* 스크롤 강제 차단 */
-}
-
 .home-container {
   width: 100vw;
   height: 100vh;
   overflow: hidden;
-  display: flex; /* flex로 내부 요소 높이 강제 조정 */
-  flex-direction: column;
-}
-
-.home-container > div {
-  flex: 1;
 }
 </style>
